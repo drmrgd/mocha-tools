9b63f743a6d51dc72fbb9e87e9ba57795dbbc3c0
Dave Sims <misc@lamneth.net>
2013-11-22 13:32:00 -0500

    varCollector.sh Driver Script Update - Added R&D server CLI option rather than hardcoded method - Fixed small bits that wouldn't conform to TVCv4.0 input. - Changed sub script calls to work from package directory rather than   rely on symlinked binaries - Changed archive method to .zip rather than tarball to be closer to   plugin output - Fixed up misc bits of less than stellar code

--- varCollector/varCollector.sh
+++ varCollector/varCollector.sh
@@ -13,7 +13,7 @@
 
 #set -x
 
-VERSION="$(basename $0) - v0.8.2"
+VERSION="$(basename $0) - v2.0.0"
 USAGE="$(cat <<EOT
 $VERSION [options]
 
@@ -23,6 +23,7 @@ Caller (TVC) plugin has been run.
 
     -l    Non-default lookup file for the cpscChecker utility (default: 3)
     -s    Custom sampleKey file rather than the default based on the ion_params_00.json file
+    -r    Run plugin without cpscChecker (R&D Server without standard sample IDs)
     -v    Version information
     -h    This help text
 
@@ -37,7 +38,11 @@ cpscSample="IonXpress_001.txt"
 cpsc_lookup=3 #Default lookup file for cpscChecker (see cpscChecker for inforamation)
 is_RandD_server=0 #Change to 0 for production server with locked pipeline.
 
-while getopts :hvs:l: opt;
+# Get absolute path of scriptname in order to be more flexible later. 
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=$(dirname $SCRIPT)
+
+while getopts :hvrs:l: opt;
 do
 	case $opt in
 		v)
@@ -55,6 +60,9 @@ do
 				exit 1
 			fi
 			;;
+        r)
+            is_RandD_server=1
+            ;;
 		l)
 			cpsc_lookup="$OPTARG"
 			;;
@@ -72,9 +80,10 @@ do
 done
 shift $((OPTIND - 1))
 
+
 # Check to make sure TVC has been run and you are in the results dir
 if [ ! -d "$TVCout" ]; then
-	printf "ERROR: Either you are not running this script from a run results folder, or TVC has not been run on this sample.\n"
+	echo "[ ERROR ] Either you are not running this script from a run results folder, or TVC has not been run on this sample."
 	exit 1
 fi
 
@@ -90,12 +99,14 @@ if [ -d "$colvarsDir" ]; then
 fi
 
 # Make collectedVariants directory and run sampleKeyGen.pl to generate a sample key for the run
+echo "Making a collectedVariants dir..."
 mkdir $colvarsDir;
 # Use custom key instead of generating one and copy to $colvarsDir
 if [[ $customKey ]]; then
 	cp $customKey $colvarsDir/
 else
-	eval "sampleKeyGen -o $colvarsDir/sampleKey.txt /opt/mocha/varCollector/resources/bcIndex.txt ion_params_00.json" 
+    echo "Generating sampleKey.txt..."
+    eval "perl $SCRIPTPATH/sampleKeyGen.pl -o $colvarsDir/sampleKey.txt" 
 	if [[ $? -ne 0 ]]; then
 		echo "The sampleKeyGen tool had a problem and failed to run correctly!" 
 		exit 1
@@ -103,38 +114,37 @@ else
 fi
 
 # Run collectVariants.pl
-printf "Running collectVariants.pl on TVC results.  See output in 'collectedVariants'\n"
-# TODO: use relative package path.
-eval "perl /opt/mocha/varCollector/collectVariants.pl" 
+echo "Running collectVariants.pl on TVC results..." 
+eval "perl $SCRIPTPATH/collectVariants.pl" 
 if [[ $? -ne 0 ]]; then
-	echo "The collectVariants.pl script had problems and  failed to run!" 
+	echo "[ ERROR ] The collectVariants.pl script had problems and failed to run!" 
 	exit 1
 fi
 
 if [ ! -d "$colvarsDir" ]; then 
-	printf "ERROR: no 'collectedVariants' directory found.  Something bad happened!\n"
+	echo "[ ERROR ] No 'collectedVariants' directory found. Something bad happened!"
 	exit 1
 fi
 
 if [[ $is_RandD_server -eq 1 ]]; then
-	printf "Running in an R&D environment.  No automatic cpscChecker run.  Please run the cpscChecker tool manually\n"
+	echo "[ NOTE ]: Running in an R&D environment.  No automatic cpscChecker run.  Please run the cpscChecker tool manually."
 else
 	# Verify conditions are correct for cpscChecker 
 	if [ ! -f "$colvarsDir/sampleKey.txt" ]; then
-		printf "ERROR: No sampleKey file found.  Can not run cpscChecker!\n"
+		printf "[ ERROR ] No sampleKey file found.  Can not run cpscChecker!\n"
 	elif [ ! -f "$colvarsDir/IonXpress_001.txt" ]; then
-		printf "ERROR: no "$cpscSample" file found.  Did you run a CPSC sample?\n";
+		printf "[ ERROR ] No "$cpscSample" file found.  Did you run a CPSC sample?\n";
 		exit 1
 	fi	
 		
 	#  If conditions are OK, let's run the script.
 	cd $colvarsDir
 	printf "Running cpscChecker for '$runName' on sample '$cpscSample'...\n"
-	eval "cpscChecker -l $cpsc_lookup $cpscSample"
+	eval "perl $SCRIPTPATH/../cpscChecker/cpscChecker.pl -l $cpsc_lookup $cpscSample"
 fi
 
-# Prepare a tarball of results for experiment folder 
+# Prepare a zip archive of results for experiment folder 
 cd $colvarsDir
-tar vcfz "$runName"_variants.tar.gz * > /dev/null 2>&1
+zip "$runName"_variants.zip * > /dev/null 2>&1
 
-printf "\nScript complete.  Check results in 'collectedVariants' directory\n"
+echo -e "\nScript complete. Results written to the 'collectedVariants' directory\n" 
